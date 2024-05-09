b4ba18aa3778b3f33d293b74d798fa6ffcdf334b
vik673 <vjvikas66@gmail.com>
2024-05-09 22:07:02 +0530

    new file added

--- ConvertTemperature.java
+++ ConvertTemperature.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class ConvertTemperature{
 	public static void main(String args[])
 	{
+		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the Temperature(in celsius)");
 		Double C=sc.nextDouble();
